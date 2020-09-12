3ae8ccfa573240737cab8ee376d9971d618b0d6a
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-09-12 23:15:58 +0900

    initial commit

---
+++ src/main/java/chiharu/hagihara/waterfallmckotlintemplate/WaterfallMCKotlinTemplate.java
@@ -0,0 +1,16 @@
+package chiharu.hagihara.waterfallmckotlintemplate;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class WaterfallMCKotlinTemplate extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
